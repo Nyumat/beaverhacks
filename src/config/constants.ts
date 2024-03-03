70f15a49bb489408493bf744646149800f3eceb9
Thomas <nyumat18@gmail.com>
2024-03-03 05:13:46 -0800

    categories as constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
+import { Category } from "@/types";
+
 export const notes = [
   "C",
   "C#",
@@ -44,4 +46,36 @@ export const keys = [
   ",",
   ".",
   "/",
+];
+
+export const categories: Category[] = [
+  {
+    name: "Kicks",
+    samples: [
+      { url: "/0/kick.wav", name: "808 Kick" },
+      { url: "/0/kick2.wav", name: "808 Kick 2" },
+    ],
+  },
+  {
+    name: "Hats",
+    samples: [
+      { url: "/0/hat.wav", name: "Closed Hat" },
+      { url: "/0/ohat.wav", name: "Open Hat" },
+    ],
+  },
+  {
+    name: "Snares",
+    samples: [{ url: "/0/snare.wav", name: "Snare" }],
+  },
+  {
+    name: "Toms",
+    samples: [
+      { url: "/0/tom.wav", name: "Tom" },
+      { url: "/0/tom2.wav", name: "Tom 2" },
+    ],
+  },
+  {
+    name: "Percussion",
+    samples: [{ url: "/0/clap.wav", name: "Clap" }],
+  },
 ];
